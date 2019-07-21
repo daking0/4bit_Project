ac47ac3bd4936b9c8c31b7b00595884b0c28dec7
YoungbinChoo <youngbinchoo@gmail.com>
2019-07-21 15:07:21 +0900

    no message

--- src/main/java/com/example/spring/project_bit/entity/HW_File.java
+++ src/main/java/com/example/spring/project_bit/entity/HW_File.java
@@ -1,4 +1,23 @@
 package com.example.spring.project_bit.entity;
 
-public class HW_File {
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import java.io.Serializable;
+
+public class HW_File implements Serializable {
+
+    @Column(name = "HW_file_UUID")
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    private Long hwFileUUID;
+
+    @Column(name = "HW_file_url")
+    private String hwFileURL;
+
+    @Column(name = "HW_article_UUID")
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    private Long hwArticleUUID;
 }
