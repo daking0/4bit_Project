a7d7c2f7010e261f870ecc9813122b00ffaeddec
YoungbinChoo <youngbinchoo@gmail.com>
2019-07-29 15:54:54 +0900

    no message

--- src/main/java/com/example/spring/project_bit/controller/MainController.java
+++ src/main/java/com/example/spring/project_bit/controller/MainController.java
@@ -4,11 +4,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/main")
+@RequestMapping("/main/mypage")
 public class MainController {
 
-
-    public void mainPage(){
-        //공지게시판, 반공지게시판, 취업게시판 읽어오기
+    @RequestMapping(path = "/attend")
+    public void MyAttend(){
+        //출결확인
     }
+
+    @RequestMapping(path = "/myinfo")
+    public void myInfo(){
+        //내정보보기
+    }
+
+    @RequestMapping(path = "/myinfo/edit")
+    public void myInfoEdit(){
+        //출결확인
+    }
+
+
+    @RequestMapping(path = "/mypoint")
+    public void mainMyPage(){
+        //출결확인
+    }
+
 }
