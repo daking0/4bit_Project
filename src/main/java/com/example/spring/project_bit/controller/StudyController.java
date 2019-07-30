8ba634c8a17ebbe5e83f830b020a03701c9320f8
이채연 <codusaa98@hanmail.net>
2019-07-30 14:59:25 +0900

    no message

--- src/main/java/com/example/spring/project_bit/controller/StudyController.java
+++ src/main/java/com/example/spring/project_bit/controller/StudyController.java
@@ -14,21 +14,6 @@ public class StudyController {
         // 로드맵 리스트
     }
 
-    @RequestMapping("/endedtest")
-    public void endedTest(int testId, String id){
-        // 지난 시험
-    }
-
-    @RequestMapping("/plustest")
-    public void plusTest(int testId){
-        // 추가 문제
-    }
-
-    @RequestMapping("/endedassignment")
-    public void endedAssignment(){
-        // 지난 과제
-    }
-
     @RequestMapping("studyentstatus")
     public void studentStatus(){
         // 학생 현황 >> 강사만
